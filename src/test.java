6cade38bd947318917012e66b5ed31b31e52d55f
[Jackson Paris] <[jparis@zagmail.gonzaga.edu]>
2019-08-29 10:12:26 -0700

    [added a third println]

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("My second hello world");
         System.out.println("ahhhhhhhhh");
+        System.out.println("push dis to the repo");
     }
 }
